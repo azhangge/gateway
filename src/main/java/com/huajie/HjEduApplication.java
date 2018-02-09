0c613c7e9a98cc143bd355b1f74fd54371704360
zhanggz <1007009258@qq.com>
2018-02-09 11:14:43 +0800

    solr集成

--- src/main/java/com/huajie/HjEduApplication.java
+++ src/main/java/com/huajie/HjEduApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -18,7 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 
 @SpringBootApplication
-@Controller
+@EnableAutoConfiguration
 public class HjEduApplication {
 
     @Autowired
